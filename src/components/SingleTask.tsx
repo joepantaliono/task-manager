860d504b41e69ce0cf639caa0ac8917cf42aad90
Joseph Pantaliono <pantalionojoseph@gmail.com>
2022-02-06 17:27:13 -0600

    add editing tasks

--- src/components/SingleTask.tsx
+++ src/components/SingleTask.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Task } from '../model';
 import { MdDelete, MdDone, MdEdit } from 'react-icons/md';
 import './styles.css';
-import TaskList from './TaskList';
 
 interface Props {
     task:Task;
@@ -11,23 +10,57 @@ interface Props {
 };
 
 const SingleTask = ({ task, taskList, setTaskList }: Props) => {
+    const [edit, setEdit] = useState<boolean>(false);
+    const [editTask, setEditTask] = useState<string>(task.task);
+
+
     const handleComplete = (id: number) => {
         setTaskList(taskList.map(task=> task.id===id?{...task, completed:!task.completed}:task))
-    }
+    };
 
     const handleDelete = (id: number) => {
         setTaskList(taskList.filter((task)=> task.id !== id))
-    }
+    };
+    
+    const inputRef = useRef<HTMLInputElement>(null);
+    useEffect(() => {
+      inputRef.current?.focus();
+    }, [edit]);
 
-  return <form className='single-task'>
-      {task.completed? (
-          <s className="single-task-text">{task.task}</s>
-      ):(
-        <span className="single-task-text">{task.task}</span>
-      )}
+    const handleEdit = (e: React.FormEvent, id: number) => {
+        e.preventDefault();
+        setTaskList(taskList.map((task)=>(task.id===id? { ...task, task: editTask }: task)));
+        setEdit(false);
+      };
+
+  return <form className='single-task' onSubmit={(e)=>handleEdit(e, task.id)}>
+      {
+          edit? (
+              <input 
+              ref={inputRef}
+              value={editTask}
+              onChange={(e) => setEditTask(e.target.value)}
+              className="single-task-text"
+              />
+          ):(
+            task.completed? (
+                <s className="single-task-text">{task.task}</s>
+            ):(
+              <span className="single-task-text">{task.task}</span>
+            )
+          )
+      }
       
       <div>
-          <span className='icon'><MdEdit /></span>
+          <span className='icon'
+          onClick={() => {
+              if (!edit && !task.completed){
+                setEdit(!edit);
+              }}
+          }
+            >
+                <MdEdit />
+                </span>
           <span className='icon' onClick={()=>handleDelete(task.id)}><MdDelete /></span>
           <span className='icon' onClick={()=>handleComplete(task.id)}><MdDone /></span>
       </div>
