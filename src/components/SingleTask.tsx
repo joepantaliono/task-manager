f3ea149396d9aa9d8e0fd0e6a7e883c13c1f2c8e
Joseph Pantaliono <pantalionojoseph@gmail.com>
2022-02-06 12:25:57 -0600

    handleComplete, handleDelete

--- src/components/SingleTask.tsx
+++ src/components/SingleTask.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Task } from '../model';
 import { MdDelete, MdDone, MdEdit } from 'react-icons/md';
 import './styles.css';
+import TaskList from './TaskList';
 
 interface Props {
     task:Task;
@@ -10,12 +11,25 @@ interface Props {
 };
 
 const SingleTask = ({ task, taskList, setTaskList }: Props) => {
+    const handleComplete = (id: number) => {
+        setTaskList(taskList.map(task=> task.id===id?{...task, completed:!task.completed}:task))
+    }
+
+    const handleDelete = (id: number) => {
+        setTaskList(taskList.filter((task)=> task.id !== id))
+    }
+
   return <form className='single-task'>
-      <span className="single-task-text">{task.task}</span>
+      {task.completed? (
+          <s className="single-task-text">{task.task}</s>
+      ):(
+        <span className="single-task-text">{task.task}</span>
+      )}
+      
       <div>
           <span className='icon'><MdEdit /></span>
-          <span className='icon'><MdDelete /></span>
-          <span className='icon'><MdDone /></span>
+          <span className='icon' onClick={()=>handleDelete(task.id)}><MdDelete /></span>
+          <span className='icon' onClick={()=>handleComplete(task.id)}><MdDone /></span>
       </div>
   </form>;
 };
